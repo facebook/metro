3529a955d1e5acf4ce2818c218c8b56eecee42b4
Jean Lauliac <jeanlauliac@fb.com>
2017-05-23 15:44:17 +0100

    add custom Promise def that accepts null arg for then()

---
+++ flow-typed/Promise.js
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow
+ * @nolint
+ */
+
+/* eslint-disable */
+
+// These annotations are copy/pasted from the built-in Flow definitions and
+// tweaked so that then() and catch() accept `null` arguments, that they
+// rightfully do. This should probably be changed in the core lib eventually.
+declare class Promise<+R> {
+  constructor(callback: (
+    resolve: (result?: Promise<R> | R) => void,
+    reject: (error?: any) => void
+  ) => mixed): void;
+
+  then<U>(
+    onFulfill?: ?(value: R) => Promise<U> | ?U,
+    onReject?: ?(error: any) => Promise<U> | ?U
+  ): Promise<U>;
+
+  catch<U>(
+    onReject?: (error: any) => ?Promise<U> | U
+  ): Promise<U>;
+
+  static resolve<T>(object?: Promise<T> | T): Promise<T>;
+  static reject<T>(error?: mixed): Promise<T>;
+
+  static all<Elem, T:Iterable<Elem>>(promises: T): Promise<$TupleMap<T, typeof $await>>;
+  static race<T>(promises: Array<Promise<T>>): Promise<T>;
+}
