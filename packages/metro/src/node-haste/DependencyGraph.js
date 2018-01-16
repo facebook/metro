dfcaa30f262b73be88c624347f3ff5cf44a61fdc
Jean Lauliac <jeanlauliac@fb.com>
2018-01-16 10:17:11 -0800

    metro: break jest-haste-map cache
    Reviewed By: cpojer, mjesun
    
    Differential Revision: D6728279
    
    fbshipit-source-id: ef26c6a295626adb6ff6272b308110fd00b096e2

--- packages/metro/src/node-haste/DependencyGraph.js
+++ packages/metro/src/node-haste/DependencyGraph.js
@@ -66,7 +66,7 @@ type Options = {|
   +watch: boolean,
 |};
 
-const JEST_HASTE_MAP_CACHE_BREAKER = 1;
+const JEST_HASTE_MAP_CACHE_BREAKER = 2;
 
 class DependencyGraph extends EventEmitter {
   _assetResolutionCache: AssetResolutionCache;
