8242229b59915a1e9873e27a542755a7d1dc1e2e
Sophie Alpert <sophiebits@fb.com>
2018-02-16 18:24:55 -0800

    Update license headers for MIT license
    Summary:
    Includes React Native and its dependencies Fresco, Metro, and Yoga. Excludes samples/examples/docs.
    
    find: ^(?:( *)|( *(?:[\*~#]|::))( )? *)?Copyright (?:\(c\) )?(\d{4})\b.+Facebook[\s\S]+?BSD[\s\S]+?(?:this source tree|the same directory)\.$
    replace: $1$2$3Copyright (c) $4-present, Facebook, Inc.\n$2\n$1$2$3This source code is licensed under the MIT license found in the\n$1$2$3LICENSE file in the root directory of this source tree.
    
    Reviewed By: TheSavior, yungsters
    
    Differential Revision: D7007050
    
    fbshipit-source-id: 37dd6bf0ffec0923bfc99c260bb330683f35553e

--- packages/metro/src/babel-bridge.js
+++ packages/metro/src/babel-bridge.js
@@ -1,10 +1,8 @@
 /**
  * Copyright (c) 2015-present, Facebook, Inc.
- * All rights reserved.
  *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  *
  * @emails oncall+javascript_foundation
  * @flow (won't like this)
@@ -111,11 +109,9 @@ function makeMakeHMRConfig7() {
   // from: babel-preset-react-native/configs/hmr
   /**
    * Copyright (c) 2015-present, Facebook, Inc.
-   * All rights reserved.
    *
-   * This source code is licensed under the BSD-style license found in the
-   * LICENSE file in the root directory of this source tree. An additional grant
-   * of patent rights can be found in the PATENTS file in the same directory.
+   * This source code is licensed under the MIT license found in the
+   * LICENSE file in the root directory of this source tree.
    */
   'use strict';
 
@@ -156,12 +152,9 @@ function getPreset7() {
   // from: fbsource/xplat/js/node_modules/babel-preset-react-native/configs/main.js
   /**
    * Copyright (c) 2015-present, Facebook, Inc.
-   * All rights reserved.
    *
-   * This source code is licensed under the BSD-style license found in the
-   * LICENSE file in the root directory of this source tree. An additional grant
-   * of patent rights can be found in the PATENTS file in the same directory.
-   *
+   * This source code is licensed under the MIT license found in the
+   * LICENSE file in the root directory of this source tree.
    */
   'use strict';
 
@@ -260,7 +253,10 @@ function transformSymbolMember() {
   // from: fbsource/xplat/js/node_modules/babel-preset-react-native/transforms/transform-symbol-member.js
 
   /**
-   * Copyright 2004-present Facebook. All Rights Reserved.
+   * Copyright (c) 2015-present, Facebook, Inc.
+   *
+   * This source code is licensed under the MIT license found in the
+   * LICENSE file in the root directory of this source tree.
    */
 
   'use strict';
@@ -327,12 +323,10 @@ function transformDynamicImport() {
   // from: fbsource/xplat/js/node_modules/babel-preset-react-native/transforms/transform-dynamic-import.js
 
   /**
-   * Copyright (c) 2016-present, Facebook, Inc.
-   * All rights reserved.
+   * Copyright (c) 2015-present, Facebook, Inc.
    *
-   * This source code is licensed under the BSD-style license found in the
-   * LICENSE file in the root directory of this source tree. An additional grant
-   * of patent rights can be found in the PATENTS file in the same directory.
+   * This source code is licensed under the MIT license found in the
+   * LICENSE file in the root directory of this source tree.
    */
 
   'use strict';
@@ -366,11 +360,9 @@ function getBabelRegisterConfig7() {
 
   /**
    * Copyright (c) 2015-present, Facebook, Inc.
-   * All rights reserved.
    *
-   * This source code is licensed under the BSD-style license found in the
-   * LICENSE file in the root directory of this source tree. An additional grant
-   * of patent rights can be found in the PATENTS file in the same directory.
+   * This source code is licensed under the MIT license found in the
+   * LICENSE file in the root directory of this source tree.
    *
    * @format
    */
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -1,7 +1,6 @@
 /*eslint sort-keys: 0*/
 /**
  * Copyright (c) 2017-present, Facebook, Inc.
- * All rights reserved.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
