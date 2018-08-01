37f2c3c5ab906711b898d7158365627e3298e1e5
Linus Unnebäck <linus@folkdatorn.se>
2018-08-01 19:51:25 +0100

    Add a TypeScript file to the integration test

---
+++ packages/metro/src/integration_tests/basic_bundle/TypeScript.ts
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) 2018-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+export const type = 'TypeScript' as string
+export const test = true as boolean
