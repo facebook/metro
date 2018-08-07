e591dc72724d5a381f4b054a4c2ddf30960baf2b
Rafael Oleza <rafeca@fb.com>
2018-08-07 06:27:54 -0700

    Simplify basic bundle test
    Reviewed By: pvdz
    
    Differential Revision: D9050068
    
    fbshipit-source-id: cbf8d5f5c5f107e042a52b69c56ea4deaadd9fb1

--- packages/metro/src/integration_tests/__tests__/build-test.js
+++ packages/metro/src/integration_tests/__tests__/build-test.js
@@ -5,16 +5,22 @@
  * LICENSE file in the root directory of this source tree.
  *
  * @emails oncall+javascript_foundation
+ * @flow
  * @format
  */
 
 'use strict';
 
 const Metro = require('../../..');
+const MetroConfig = require('metro-config');
+
+const path = require('path');
 
 jest.unmock('cosmiconfig');
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 30 * 1000;
+
+const INPUT_PATH = path.resolve(__dirname, '../basic_bundle');
 
 it('builds a simple bundle', async () => {
   const config = await Metro.loadConfig({
@@ -26,4 +32,23 @@ it('builds a simple bundle', async () => {
   });
 
   expect(result.code).toMatchSnapshot();
+});
+
+it('build a simple bundle with polyfills', async () => {
+  const polyfill1 = path.join(INPUT_PATH, 'polyfill-1.js');
+  const polyfill2 = path.join(INPUT_PATH, 'polyfill-2.js');
+
+  const baseConfig = await Metro.loadConfig({
+    config: require.resolve('../metro.config.js'),
+  });
+  const config = MetroConfig.mergeConfig(baseConfig, {
+    serializer: {
+      polyfillModuleNames: [polyfill1, polyfill2],
+    },
+  });
+
+  const bundleWithPolyfills = await Metro.runBuild(config, {
+    entry: 'TestBundle.js',
+  });
+  expect(bundleWithPolyfills.code).toMatchSnapshot();
 });
