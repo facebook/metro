d04360b35acdfd484a13d2a0e0cce249f280fd33
Ives van Hoorne <ives@fb.com>
2018-09-10 07:11:33 -0700

    Add AutoCleanFileStore as a cache store option
    Reviewed By: mjesun
    
    Differential Revision: D9660824
    
    fbshipit-source-id: 07715b26d8dbd6fa9f12a37bf9a64618815f062d

--- packages/metro-cache/src/index.js
+++ packages/metro-cache/src/index.js
@@ -10,6 +10,7 @@
 
 'use strict';
 
+const AutoCleanFileStore = require('./stores/AutoCleanFileStore');
 const Cache = require('./Cache');
 const FileStore = require('./stores/FileStore');
 const HttpStore = require('./stores/HttpStore');
@@ -20,6 +21,7 @@ export type {Options as FileOptions} from './stores/FileStore';
 export type {Options as HttpOptions} from './stores/HttpStore';
 export type {CacheStore} from './types.flow';
 
+module.exports.AutoCleanFileStore = AutoCleanFileStore;
 module.exports.Cache = Cache;
 module.exports.FileStore = FileStore;
 module.exports.HttpStore = HttpStore;
---
+++ packages/metro-cache/src/stores/__tests__/AutoCleanFileStore-test.js
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2018-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @emails oncall+javascript_foundation
+ * @format
+ * @flow
+ */
+
+'use strict';
+
+describe('AutoCleanFileStore', () => {
+  let AutoCleanFileStore;
+  let fs;
+
+  beforeEach(() => {
+    jest
+      .resetModules()
+      .resetAllMocks()
+      .useFakeTimers()
+      .mock('fs', () => new (require('metro-memory-fs'))());
+
+    AutoCleanFileStore = require('../AutoCleanFileStore');
+    fs = require('fs');
+    jest.spyOn(fs, 'unlinkSync');
+  });
+
+  it('sets and writes into the cache', () => {
+    const fileStore = new AutoCleanFileStore({
+      root: '/root',
+      intervalMs: 49,
+      cleanupThresholdMs: 0,
+    });
+    const cache = Buffer.from([0xfa, 0xce, 0xb0, 0x0c]);
+
+    fileStore.set(cache, {foo: 42});
+    expect(fileStore.get(cache)).toEqual({foo: 42});
+
+    jest.runTimersToTime(30);
+
+    expect(fileStore.get(cache)).toEqual({foo: 42});
+
+    jest.runTimersToTime(40);
+
+    // mtime doesn't work very well in in-memory-store, so we couldn't test that
+    // functionality
+    expect(fileStore.get(cache)).toEqual(null);
+  });
+
+  it('returns null when reading a non-existing file', () => {
+    const fileStore = new AutoCleanFileStore({root: '/root'});
+    const cache = Buffer.from([0xfa, 0xce, 0xb0, 0x0c]);
+
+    expect(fileStore.get(cache)).toEqual(null);
+  });
+});
