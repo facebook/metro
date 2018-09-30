2550d49a4cba8fff4aafc78f98112c49b6980e20
simonpfish <simonpfish@gmail.com>
2018-09-29 18:13:45 -0700

    Create simple development script for metro-visualizer
    The idea is to run a simple metro server on the visualizer codebase that can be used to test the visualizer UI itself.

---
+++ packages/metro-visualizer/metro.config.js
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @emails oncall+javascript_foundation
+ * @format
+ */
+
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+let resolveRequest;
+let getWatchFolders;
+try {
+  /* eslint-disable import/no-extraneous-dependencies */
+  resolveRequest = require('@xplatjs/metro-scripts/resolver');
+  getWatchFolders = require('@xplatjs/metro-scripts/get-watch-folders');
+  /* eslint-enable import/no-extraneous-dependencies */
+} catch (e) {
+  resolveRequest = undefined;
+  getWatchFolders = () => [];
+}
+
+module.exports = {
+  dev: true,
+  resolver: {
+    resolveRequest,
+  },
+  projectRoot: fs.realpathSync(path.resolve(__dirname, './')),
+  watchFolders: getWatchFolders(),
+  server: {port: 8080, enableVisualizer: true},
+  transformer: {
+    babelTransformerPath: require.resolve(
+      './src/middleware/build-utils/transformer',
+    ),
+    workerPath: require.resolve('./src/middleware/build-utils/transformWorker'),
+  },
+};
