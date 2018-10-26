ec29084e558b9d4c1e23b1d1c7b1c6afa2571eb0
David Aurelio <davidaurelio@fb.com>
2018-10-26 10:04:25 -0700

    Add binary serialization format
    Summary:
    Adds a binary delta serialization format that can be consumed as stream, i.e. incrementally.
    
    The benefit is that we don’t have to allocate contiguous memory for JSON parsers in Java / C++ (folly), and can copy code directly into the map holding memory.
    
    Reviewed By: rafeca
    
    Differential Revision: D10405570
    
    fbshipit-source-id: 6f6bda4e54164f03176fd0a5d9687ab05e4abc22

--- packages/metro/src/DeltaBundler/Serializers/helpers/__tests__/serializeDeltaJSBundle-test.js
+++ packages/metro/src/DeltaBundler/Serializers/helpers/__tests__/serializeDeltaJSBundle-test.js
@@ -11,6 +11,7 @@
 
 'use strict';
 
+const crc32 = (require('buffer-crc32'): {unsigned(Buffer): number});
 const serializeDeltaJSBundle = require('../serializeDeltaJSBundle');
 
 const deltaBundle = {
@@ -31,3 +32,141 @@ it('can serialize to a string', () => {
     JSON.stringify(deltaBundle),
   );
 });
+
+const SIZEOF_UINT32 = 4;
+const EMPTY_STRING = 0xffffffff;
+
+describe('binary stream serialization', () => {
+  const VERSION_OFFSET = 4;
+  const RESET_OFFSET = VERSION_OFFSET + 3;
+  const BODY_OFFSET = RESET_OFFSET + 1;
+
+  const EMPTY_CRC32 = 0;
+
+  const stream = serializeDeltaJSBundle.toBinaryStream(deltaBundle);
+  const serialized = consumeStream(stream);
+
+  let buffer;
+  beforeEach(async () => {
+    buffer = await serialized;
+  });
+
+  const subBuffer = (offset, length) => buffer.slice(offset, offset + length);
+
+  it('starts with the magic number', () => {
+    expect(subBuffer(0, SIZEOF_UINT32)).toEqual(
+      Buffer.of(0xfb, 0xde, 0x17, 0xa5),
+    );
+  });
+
+  it('contains `1` as format version number', () => {
+    //eslint-disable-next-line no-bitwise
+    expect(buffer.readUInt32LE(VERSION_OFFSET) & 0xffffff).toEqual(1);
+  });
+
+  it('has the reset flag set', () => {
+    expect(buffer.readUInt8(RESET_OFFSET)).toEqual(1);
+  });
+
+  const expectedSequenceId = binString(deltaBundle.id);
+  it('contains the sequence ID after the magic number', () => {
+    expect(subBuffer(BODY_OFFSET, expectedSequenceId.length)).toEqual(
+      expectedSequenceId,
+    );
+  });
+
+  const preOffset = BODY_OFFSET + expectedSequenceId.length;
+  const expectedPre = preOrPostSection(deltaBundle.pre);
+  it('has data for pre-scripts', () => {
+    expect(subBuffer(preOffset, expectedPre.length)).toEqual(expectedPre);
+  });
+
+  const postOffset = preOffset + expectedPre.length;
+  const expectedPost = preOrPostSection(deltaBundle.post);
+  it('has data for post-scripts', () => {
+    expect(subBuffer(postOffset, expectedPost.length)).toEqual(expectedPost);
+  });
+
+  const modulesOffset = postOffset + expectedPost.length;
+  it('has module data', () => {
+    expect(buffer.slice(modulesOffset)).toEqual(
+      Buffer.concat(deltaBundle.delta.map(binModule)),
+    );
+  });
+
+  describe('empty pre or post scripts, reset = false:', () => {
+    const stream = serializeDeltaJSBundle.toBinaryStream({
+      ...deltaBundle,
+      pre: [],
+      post: [],
+      reset: false,
+    });
+    const serialized = consumeStream(stream);
+
+    let buffer, preOffset;
+    beforeEach(async () => {
+      buffer = await serialized;
+      preOffset =
+        BODY_OFFSET + SIZEOF_UINT32 + buffer.readUInt32LE(BODY_OFFSET);
+    });
+
+    it('has a reset byte of 0', () => {
+      expect(buffer.readUInt8(RESET_OFFSET)).toEqual(0);
+    });
+
+    it('contains no pre scripts', () => {
+      expect(buffer.readUInt32LE(preOffset)).toEqual(EMPTY_STRING);
+      expect(buffer.readUInt32LE(preOffset + SIZEOF_UINT32)).toEqual(
+        EMPTY_CRC32,
+      );
+    });
+
+    it('contains no post scripts', () => {
+      expect(buffer.readUInt32LE(preOffset + SIZEOF_UINT32 * 2)).toEqual(
+        EMPTY_STRING,
+      );
+      expect(buffer.readUInt32LE(preOffset + SIZEOF_UINT32 * 3)).toEqual(
+        EMPTY_CRC32,
+      );
+    });
+  });
+});
+
+function consumeStream(stream): Promise<Buffer> {
+  const chunks = [];
+  return new Promise((resolve, reject) => {
+    stream
+      .on('data', chunk => chunks.push(chunk))
+      .on('error', reject)
+      .on('end', () => resolve(Buffer.concat(chunks)));
+  });
+}
+
+function binUint32LE(value) {
+  const b = Buffer.alloc(SIZEOF_UINT32);
+  b.writeUInt32LE(value, 0);
+  return b;
+}
+
+function binString(value) {
+  const length = Buffer.byteLength(value);
+  const buffer = Buffer.alloc(length + SIZEOF_UINT32); // extra space for size
+  buffer.writeUInt32LE(length, 0);
+  buffer.write(value, SIZEOF_UINT32, length, 'utf8');
+  return buffer;
+}
+
+function binModule([id, code]: [number, ?string]) {
+  const idAndCodeBuffer = Buffer.concat([
+    binUint32LE(id),
+    code == null ? binUint32LE(EMPTY_STRING) : binString(code),
+  ]);
+  const crc32Buffer = binUint32LE(crc32.unsigned(idAndCodeBuffer));
+  return Buffer.concat([idAndCodeBuffer, crc32Buffer]);
+}
+
+function preOrPostSection(section) {
+  const source = section.map(([, code]) => `${code}\n`).join('');
+  const serialized = binString(source);
+  return Buffer.concat([serialized, binUint32LE(crc32.unsigned(serialized))]);
+}
--- packages/metro/src/DeltaBundler/Serializers/helpers/serializeDeltaJSBundle.js
+++ packages/metro/src/DeltaBundler/Serializers/helpers/serializeDeltaJSBundle.js
@@ -10,6 +10,106 @@
 
 'use strict';
 
+const crc32 = (require('buffer-crc32'): {unsigned(Buffer): number});
+const {Readable} = require('stream');
+
 import {type DeltaBundle} from '../deltaJSBundle';
 
 exports.toJSON = (JSON.stringify: DeltaBundle => string);
+
+// binary streaming format for delta bundles:
+// FB DE 17 A5     magic number
+// uint24 format version (1)
+// bool reset
+// uint32 sequenceIdLength of sequenceId
+// char[sequenceIdLength] sequenceId
+// uint32 preLength: length of "pre" section
+// char[preLength] pre section
+// char[4] pre section crc32
+// uint32 postLength : lengthof "post" section
+// char[postLength] post section
+// module[], where module = {uint32 id, uint32 length, char[length] code, crc32(code)}
+
+exports.toBinaryStream = (deltaBundle: DeltaBundle): Readable => {
+  const gen = streamDeltaBundle(deltaBundle);
+
+  return new Readable({
+    read() {
+      const {value = null} = gen.next();
+      this.push(value);
+    },
+  });
+};
+
+const MAGIC_NUMBER = Buffer.of(0xfb, 0xde, 0x17, 0xa5);
+const FORMAT_VERSION = [0x01, 0x00, 0x00];
+
+function* streamDeltaBundle(deltaBundle) {
+  yield MAGIC_NUMBER;
+  yield Buffer.of(...FORMAT_VERSION, deltaBundle.reset ? 1 : 0);
+
+  yield str(deltaBundle.id);
+  yield preOrPostSection(deltaBundle.pre);
+  yield preOrPostSection(deltaBundle.post);
+
+  for (const m of deltaBundle.delta) {
+    yield module(m);
+  }
+}
+
+const SIZEOF_UINT32 = 4;
+
+function str(value) {
+  const size = Buffer.byteLength(value);
+  const buffer = Buffer.allocUnsafe(size + SIZEOF_UINT32);
+  buffer.writeUInt32LE(size, 0);
+  buffer.write(value, SIZEOF_UINT32, size, 'utf8');
+  return buffer;
+}
+
+const ABSENT_VALUE = 0xffffffff;
+const ABSENT_BUFFER = [0xff, 0xff, 0xff, 0xff];
+const EMPTY_CRC32 = [0x00, 0x00, 0x00, 0x00];
+const EMPTY_PRE_OR_POST_SECTION = Buffer.of(...ABSENT_BUFFER, ...EMPTY_CRC32);
+
+function preOrPostSection(section) {
+  if (section.length === 0) {
+    return EMPTY_PRE_OR_POST_SECTION;
+  }
+
+  const sectionCode = section.map(x => x[1]).join('\n') + '\n';
+  const size = Buffer.byteLength(sectionCode, 'utf8');
+
+  const buffer = Buffer.allocUnsafe(size + SIZEOF_UINT32 * 2); // space for size and checksum
+  buffer.writeUInt32LE(size, 0);
+  buffer.write(sectionCode, SIZEOF_UINT32, size, 'utf8');
+
+  appendCRC32(buffer);
+
+  return buffer;
+}
+
+function module(idAndCode) {
+  const code = idAndCode[1];
+  let buffer, length;
+  if (code == null) {
+    length = ABSENT_VALUE;
+    buffer = Buffer.allocUnsafe(SIZEOF_UINT32 * 3); // id, length, crc32
+  } else {
+    length = Buffer.byteLength(code, 'utf8');
+    buffer = Buffer.allocUnsafe(length + SIZEOF_UINT32 * 3);
+    buffer.write(code, SIZEOF_UINT32 * 2, length, 'utf8');
+  }
+  buffer.writeUInt32LE(idAndCode[0], 0);
+  buffer.writeUInt32LE(length, SIZEOF_UINT32);
+  appendCRC32(buffer);
+  return buffer;
+}
+
+function appendCRC32(buffer) {
+  const CRC32_OFFSET = buffer.length - SIZEOF_UINT32;
+  buffer.writeUInt32LE(
+    crc32.unsigned(buffer.slice(0, CRC32_OFFSET)),
+    CRC32_OFFSET,
+  );
+}
