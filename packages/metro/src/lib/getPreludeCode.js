2fe29220777f0b3c316e867e3813215c0f0e8280
Miguel Jimenez Esun <mjimenez@fb.com>
2019-02-26 08:25:25 -0800

    Improve prelude code
    Reviewed By: jeanlauliac
    
    Differential Revision: D14184962
    
    fbshipit-source-id: c4789e8bb3b570d82311861126043f5d0e6deadd

--- packages/metro/src/lib/getPreludeCode.js
+++ packages/metro/src/lib/getPreludeCode.js
@@ -18,9 +18,9 @@ function getPreludeCode({
   +isDev: boolean,
 |}): string {
   const vars = [
-    ...formatExtraVars(extraVars),
-    `__DEV__=${String(isDev)}`,
     '__BUNDLE_START_TIME__=this.nativePerformanceNow?nativePerformanceNow():Date.now()',
+    `__DEV__=${String(isDev)}`,
+    ...formatExtraVars(extraVars),
     'process=this.process||{}',
   ];
   return `var ${vars.join(',')};${processEnv(
@@ -28,16 +28,22 @@ function getPreludeCode({
   )}`;
 }
 
+const excluded = new Set(['__BUNDLE_START_TIME__', '__DEV__', 'process']);
+
 function formatExtraVars(extraVars) {
   const assignments = [];
+
   for (const key in extraVars) {
-    assignments.push(`${key}=${JSON.stringify(extraVars[key])}`);
+    if (extraVars.hasOwnProperty(key) && !excluded.has(key)) {
+      assignments.push(`${key}=${JSON.stringify(extraVars[key])}`);
+    }
   }
+
   return assignments;
 }
 
 function processEnv(nodeEnv) {
-  return `process.env=process.env||{};process.env.NODE_ENV=${JSON.stringify(
+  return `process.env=process.env||{};process.env.NODE_ENV=process.env.NODE_ENV||${JSON.stringify(
     nodeEnv,
   )};`;
 }
