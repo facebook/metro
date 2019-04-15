12b7fd827b9bb61ca973227b03e5c05ee598916d
Panagiotis Vekris <pvekris@fb.com>
2019-04-15 11:11:10 -0700

    Deploy 0.97.0 to xplat
    Reviewed By: dsainati1
    
    Differential Revision: D14920747
    
    fbshipit-source-id: db03f6cf73a4b3e9fad6b47e0e657490c45d06a3

--- packages/metro/src/lib/bundle-modules/registerServiceWorker.js
+++ packages/metro/src/lib/bundle-modules/registerServiceWorker.js
@@ -47,6 +47,9 @@ function registerServiceWorker(swUrl: string): void {
       serviceWorker.addEventListener('message', (event: MessageEvent) => {
         const data = event.data;
 
+        /* $FlowFixMe(>=0.97.0 site=react_native_fb) This comment suppresses an
+         * error found when Flow v0.97 was deployed. To see the error delete
+         * this comment and run Flow. */
         if (!(data instanceof Object) || typeof data.type !== 'string') {
           return;
         }
@@ -57,11 +60,20 @@ function registerServiceWorker(swUrl: string): void {
             break;
           }
           case 'METRO_UPDATE': {
+            /* $FlowFixMe(>=0.97.0 site=react_native_fb) This comment
+             * suppresses an error found when Flow v0.97 was deployed. To see
+             * the error delete this comment and run Flow. */
             console.info('Injecting metro update:', data.body);
+            /* $FlowFixMe(>=0.97.0 site=react_native_fb) This comment
+             * suppresses an error found when Flow v0.97 was deployed. To see
+             * the error delete this comment and run Flow. */
             injectUpdate(data.body);
             break;
           }
           case 'METRO_UPDATE_ERROR': {
+            /* $FlowFixMe(>=0.97.0 site=react_native_fb) This comment
+             * suppresses an error found when Flow v0.97 was deployed. To see
+             * the error delete this comment and run Flow. */
             console.error('Metro update error: ', data.error);
             break;
           }
