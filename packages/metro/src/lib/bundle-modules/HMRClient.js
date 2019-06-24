90c11e003a5c573ea8d8e7b67ecf9b71143f9415
Dan Abramov <gaearon@fb.com>
2019-06-24 09:45:29 -0700

    Make "Enable Hot Reloading" Instant
    Reviewed By: rickhanlonii
    
    Differential Revision: D15958160
    
    fbshipit-source-id: 8256fc4d5c2c3f653a78edf13b8515a5671953e4

--- packages/metro/src/lib/bundle-modules/HMRClient.js
+++ packages/metro/src/lib/bundle-modules/HMRClient.js
@@ -13,12 +13,24 @@ const WebSocketHMRClient = require('./WebSocketHMRClient');
 
 const injectUpdate = require('./injectUpdate');
 
+import type {HmrUpdate} from './types.flow';
+
 class HMRClient extends WebSocketHMRClient {
+  shouldApplyUpdates: boolean = true;
+  outdatedModules: Set<number> = new Set();
+
   constructor(url: string) {
     super(url);
 
-    this.on('update', update => {
-      injectUpdate(update);
+    this.on('update', (update: HmrUpdate) => {
+      if (this.shouldApplyUpdates) {
+        injectUpdate(update);
+      } else {
+        // Remember if there were edits while Fast Refresh is off.
+        // We'll want to warn about those modules if you turn it on.
+        update.added.forEach(([id]) => this.outdatedModules.add(id));
+        update.modified.forEach(([id]) => this.outdatedModules.add(id));
+      }
     });
   }
 }
