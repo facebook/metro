6f27878abce7286aef60f97c1b37d58d4fb7a189
Dan Abramov <gaearon@fb.com>
2019-07-18 11:05:44 -0700

    Merge stashed updates as they come in
    Reviewed By: rickhanlonii, cpojer
    
    Differential Revision: D16357065
    
    fbshipit-source-id: b8875427918342f04b8a4b4fb511c47b1fac9dae

--- packages/metro/src/lib/bundle-modules/HMRClient.js
+++ packages/metro/src/lib/bundle-modules/HMRClient.js
@@ -17,7 +17,7 @@ import type {HmrUpdate} from './types.flow';
 
 class HMRClient extends WebSocketHMRClient {
   _isEnabled: boolean = false;
-  _pendingUpdates: Array<HmrUpdate> = [];
+  _pendingUpdate: HmrUpdate | null = null;
 
   constructor(url: string) {
     super(url);
@@ -25,19 +25,21 @@ class HMRClient extends WebSocketHMRClient {
     this.on('update', (update: HmrUpdate) => {
       if (this._isEnabled) {
         injectUpdate(update);
+      } else if (this._pendingUpdate == null) {
+        this._pendingUpdate = update;
       } else {
-        // TODO: this is inefficient because we retain
-        // past versions even for the files we've edited more than once.
-        this._pendingUpdates.push(update);
+        this._pendingUpdate = mergeUpdates(this._pendingUpdate, update);
       }
     });
   }
 
   enable() {
     this._isEnabled = true;
-    const pendingUpdates = this._pendingUpdates;
-    this._pendingUpdates = [];
-    pendingUpdates.forEach(update => injectUpdate(update));
+    const update = this._pendingUpdate;
+    this._pendingUpdate = null;
+    if (update != null) {
+      injectUpdate(update);
+    }
   }
 
   disable() {
@@ -49,8 +51,85 @@ class HMRClient extends WebSocketHMRClient {
   }
 
   hasPendingUpdates() {
-    return this._pendingUpdates.length > 0;
+    return this._pendingUpdate != null;
   }
+}
+
+function mergeUpdates(base: HmrUpdate, next: HmrUpdate): HmrUpdate {
+  const addedIDs = new Set();
+  const deletedIDs = new Set();
+  const moduleMap = new Map();
+  const sourceMappingURLs = new Map();
+  const sourceURLs = new Map();
+
+  // Fill in the temporary maps and sets from both updates in their order.
+  applyUpdateLocally(base);
+  applyUpdateLocally(next);
+
+  function applyUpdateLocally(update: HmrUpdate) {
+    update.deleted.forEach(id => {
+      if (addedIDs.has(id)) {
+        addedIDs.delete(id);
+      } else {
+        deletedIDs.add(id);
+      }
+      moduleMap.delete(id);
+    });
+    update.added.forEach(([id, source], index) => {
+      if (deletedIDs.has(id)) {
+        deletedIDs.delete(id);
+      } else {
+        addedIDs.add(id);
+      }
+      moduleMap.set(id, source);
+      sourceMappingURLs.set(id, update.addedSourceMappingURLs[index]);
+      sourceURLs.set(id, update.addedSourceURLs[index]);
+    });
+    update.modified.forEach(([id, source], index) => {
+      moduleMap.set(id, source);
+      sourceMappingURLs.set(id, update.modifiedSourceMappingURLs[index]);
+      sourceURLs.set(id, update.modifiedSourceURLs[index]);
+    });
+  }
+
+  // Now reconstruct a unified update from our in-memory maps and sets.
+  // Applying it should be equivalent to applying both of them individually.
+  const result = {
+    revisionId: next.revisionId,
+    added: [],
+    addedSourceMappingURLs: [],
+    addedSourceURLs: [],
+    modified: [],
+    modifiedSourceMappingURLs: [],
+    modifiedSourceURLs: [],
+    deleted: [],
+  };
+  deletedIDs.forEach(id => {
+    result.deleted.push(id);
+  });
+  moduleMap.forEach((source, id) => {
+    if (deletedIDs.has(id)) {
+      return;
+    }
+    const sourceURL = sourceURLs.get(id);
+    const sourceMappingURL = sourceMappingURLs.get(id);
+    if (typeof sourceURL !== 'string') {
+      throw new Error('[HMRClient] Expected to find a sourceURL in the map.');
+    }
+    if (typeof sourceMappingURL !== 'string') {
+      throw new Error('[HMRClient] Expected to find a sourceURL in the map.');
+    }
+    if (addedIDs.has(id)) {
+      result.added.push([id, source]);
+      result.addedSourceMappingURLs.push(sourceMappingURL);
+      result.addedSourceURLs.push(sourceURL);
+    } else {
+      result.modified.push([id, source]);
+      result.modifiedSourceMappingURLs.push(sourceMappingURL);
+      result.modifiedSourceURLs.push(sourceURL);
+    }
+  });
+  return result;
 }
 
 module.exports = HMRClient;
