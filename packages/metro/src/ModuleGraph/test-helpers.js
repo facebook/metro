9bbef545d4bfb305bfc13922b60acff527ba268c
Micha Reiser <mreiser@fb.com>
2020-09-24 02:52:11 -0700

    Refactor optimizeDependencyMap to a babel plugin
    Reviewed By: motiz88
    
    Differential Revision: D23761585
    
    fbshipit-source-id: 7cc5b2200de1a51010d39b89ac886d3263501992

--- packages/metro/src/ModuleGraph/test-helpers.js
+++ packages/metro/src/ModuleGraph/test-helpers.js
@@ -10,7 +10,78 @@
 'use strict';
 
 const generate = require('@babel/generator').default;
+const {toMatchSnapshot} = require('jest-snapshot');
 
 const generateOptions = {concise: true, sourceType: 'module'};
-exports.codeFromAst = ast => generate(ast, generateOptions).code;
-exports.comparableCode = code => code.trim().replace(/\s+/g, ' ');
+const codeFromAst = ast => generate(ast, generateOptions).code;
+const comparableCode = code => code.trim().replace(/\s+/g, ' ');
+
+function toEqualComparableCode(received, expected) {
+  const comparableExpected = comparableCode(expected);
+
+  const pass = received === comparableExpected;
+
+  const options = {
+    isNot: this.isNot,
+    promise: this.promise,
+  };
+
+  const message = pass
+    ? () =>
+        this.utils.matcherHint(
+          'toEqualComparableCode',
+          undefined,
+          undefined,
+          options,
+        ) +
+        '\n\n' +
+        `Expected: not ${this.utils.printExpected(comparableExpected)}\n` +
+        `Received: ${this.utils.printReceived(received)}`
+    : () => {
+        const diffString = this.utils.printDiffOrStringify(
+          comparableExpected,
+          received,
+          'expected',
+          'received',
+          this.expand,
+        );
+        return (
+          this.utils.matcherHint(
+            'toEqualComparableCode',
+            undefined,
+            undefined,
+            options,
+          ) +
+          '\n\n' +
+          diffString
+        );
+      };
+
+  return {actual: received, message, pass};
+}
+
+// See https://superuser.com/questions/380772/removing-ansi-color-codes-from-text-stream
+const ANSI_PATTERN = /\x1b\[[0-9;]*m/g;
+
+function trimANSICodes(input) {
+  return input.replace(ANSI_PATTERN, '');
+}
+
+/**
+ * Matches a text that contains ANSI control codes (e.g. [39m[31m[1m^) against a snapshot.
+ * Usage: Call expect.extend({toMatchCodeFrameSnapshot}) to add the matcher in your test
+ */
+function toMatchCodeFrameSnapshot(received) {
+  return toMatchSnapshot.call(
+    this,
+    trimANSICodes(received),
+    'toMatchCodeFrameSnapshot',
+  );
+}
+
+module.exports = {
+  codeFromAst,
+  comparableCode,
+  toEqualComparableCode,
+  toMatchCodeFrameSnapshot,
+};
