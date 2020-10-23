3d564dd9fc2705cd9da86db44add9eb7be4f312d
Xuan Huang <jsx@fb.com>
2020-10-22 20:01:48 -0700

    Move Object.assign and ** to Hermes canary
    Summary: This diff moved `Object.assign` and `**` to Hemres canary channel to prepare an experiment.
    
    Reviewed By: cpojer
    
    Differential Revision: D24493905
    
    fbshipit-source-id: a39d0332c9e4c060c356b85af74cbe375f4708a5

--- packages/metro-react-native-babel-preset/src/configs/main.js
+++ packages/metro-react-native-babel-preset/src/configs/main.js
@@ -136,10 +136,10 @@ const getPreset = (src, options) => {
   if (!isHermes && (isNull || src.indexOf('`') !== -1)) {
     extraPlugins.push(es2015TemplateLiterals);
   }
-  if (isNull || src.indexOf('**') !== -1) {
+  if (!isHermesCanary && (isNull || src.indexOf('**') !== -1)) {
     extraPlugins.push(exponentiationOperator);
   }
-  if (isNull || src.indexOf('Object.assign') !== -1) {
+  if (!isHermesCanary && (isNull || src.indexOf('Object.assign')) !== -1) {
     extraPlugins.push(objectAssign);
   }
   if (hasForOf) {
