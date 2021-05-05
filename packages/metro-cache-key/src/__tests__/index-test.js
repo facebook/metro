ef3dd0a276423e1149fa2b5636a59c5a9683a4c9
Moti Zilberman <moti@fb.com>
2021-05-05 03:22:20 -0700

    Fix metro-cache-key test suite
    Summary: `metro-cache-key`'s test suite currently depends on a hash of `src/index.js`, such that any and all changes will break the test. Here we rewrite the tests in terms of a mock filesystem instead.
    
    Reviewed By: MichaReiser
    
    Differential Revision: D28120787
    
    fbshipit-source-id: 8f805d0632ab5fe6ac574229a2fcdca129886451

--- packages/metro-cache-key/src/__tests__/index-test.js
+++ packages/metro-cache-key/src/__tests__/index-test.js
@@ -11,20 +11,31 @@
 
 'use strict';
 
+jest.mock('fs', () => new (require('metro-memory-fs'))());
+
+const fs = require('fs');
 const getCacheKey = require('../index');
 
+beforeAll(() => {
+  fs.writeFileSync('/a.txt', 'dummy content for a.txt');
+  fs.writeFileSync('/copy_of_a.txt', 'dummy content for a.txt');
+  fs.writeFileSync('/b.txt', 'dummy content for b.txt');
+});
+
 test('calculates a cache key for a list of files', () => {
-  expect(getCacheKey([require.resolve('../index')])).toEqual(
-    '00138583bedb3659eeb7d68bd47ebb6d',
+  expect(getCacheKey(['/a.txt'])).toMatchInlineSnapshot(
+    `"159acfc2c1c60c655a305cb711c7bd2c"`,
   );
 
-  expect(
-    getCacheKey([require.resolve('../index'), require.resolve('ob1')]),
-  ).toEqual('d835dbaaf1d751bba4dcb1ad92f90ff9');
+  expect(getCacheKey(['/a.txt', '/b.txt'])).toMatchInlineSnapshot(
+    `"1e34c10b2663b4681858340ec5da03ce"`,
+  );
 });
 
 test('generates different keys for different files', () => {
-  expect(getCacheKey([require.resolve('../index')])).not.toEqual(
-    getCacheKey([require.resolve('ob1')]),
-  );
+  expect(getCacheKey(['/a.txt'])).not.toEqual(getCacheKey(['/b.txt']));
+});
+
+test('generates identical keys for identical files', () => {
+  expect(getCacheKey(['/a.txt'])).toEqual(getCacheKey(['/copy_of_a.txt']));
 });
