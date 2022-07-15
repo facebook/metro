c3c4308a29933990a24f9f225505d61c338a2a40
Leonard Mirt <leonardmirt@fb.com>
2022-07-15 02:58:57 -0700

    Bootstrap metro-react-native-interop-tools
    Summary:
    We’ve created a new package for interop between JS and native code.
    The main focus now is the compatibility checker, we have this tool internally, but the current implementation of the tool is quite strongly tied to Meta’s infrastructure. We’d like to explore reworking this tool to eventually make it available to the larger ecosystem.
    But the package can evolve to include more than just a compatibility checker. Maybe we can merge some older React Native codegen packages into it and have a complete interop solution based on Metro.
    
    Files added:
    	.npmignore
    	BUCK
    	package.json
    	README.md
    	src/index.js - the entrypoint of the package
    
    Reviewed By: GijsWeterings
    
    Differential Revision: D37815755
    
    fbshipit-source-id: ddd0142e671467b9a63e395e29fd438629216e72

---
+++ packages/metro-react-native-interop-tools/src/index.js
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @emails oncall+metro_bundler
+ * @flow strict-local
+ * @format
+ */
+
+export default function run(): string {
+  return 'Hello World';
+}
