7e4308eca4aebaf1b13f4174f603de7356b144ce
Moti Zilberman <moti@fb.com>
2022-08-09 13:31:31 +0100

    Add require.context integration test

---
+++ packages/metro/src/integration_tests/__tests__/require-context-test.js
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @emails oncall+metro_bundler
+ * @format
+ */
+
+'use strict';
+
+const Metro = require('../../..');
+const execBundle = require('../execBundle');
+
+jest.unmock('cosmiconfig');
+
+jest.setTimeout(30 * 1000);
+
+it('require-context/matching.js', async () => {
+  await expect(
+    execTest('require-context/matching.js'),
+  ).resolves.toMatchSnapshot();
+});
+
+it('require-context/mode-lazy.js', async () => {
+  await expect(
+    execTest('require-context/mode-lazy.js'),
+  ).resolves.toMatchSnapshot();
+});
+
+it('require-context/mode-lazy-once.js', async () => {
+  await expect(
+    execTest('require-context/mode-lazy-once.js'),
+  ).resolves.toMatchSnapshot();
+});
+
+it('require-context/mode-eager.js', async () => {
+  await expect(
+    execTest('require-context/mode-eager.js'),
+  ).resolves.toMatchSnapshot();
+});
+
+it('require-context/mode-sync.js', async () => {
+  await expect(
+    execTest('require-context/mode-sync.js'),
+  ).resolves.toMatchSnapshot();
+});
+
+async function execTest(entry) {
+  const config = await Metro.loadConfig(
+    {
+      config: require.resolve('../metro.config.js'),
+    },
+    {
+      transformer: {
+        unstable_allowRequireContext: true,
+      },
+    },
+  );
+
+  const result = await Metro.runBuild(config, {
+    entry,
+  });
+
+  return execBundle(result.code);
+}
---
+++ packages/metro/src/integration_tests/basic_bundle/require-context/matching.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import type {RequireWithContext} from './utils';
+
+declare var require: RequireWithContext;
+
+const ab = require.context('./subdir', false, /\/(a|b)\.js$/);
+const abc = require.context('./subdir', false);
+const abcd = require.context('./subdir', true);
+
+function main() {
+  return {
+    ab: ab.keys(),
+    abc: abc.keys(),
+    abcd: abcd.keys(),
+  };
+}
+
+module.exports = (main(): mixed);
