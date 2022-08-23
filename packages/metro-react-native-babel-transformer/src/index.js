a74ece0be0b284ceb0d019b8bdcf11d1fd303560
Evan Bacon <bacon@expo.io>
2022-08-23 07:03:16 -0700

    feat: load `hermes-parser` conditionally when hermes is enabled (#855)
    Summary:
    The package `hermes-parser` has a lot of JS that goes unused in web platforms, this leads to increased bundle time.
    
    Pull Request resolved: https://github.com/facebook/metro/pull/855
    
    Test Plan:
    Before
    
    ```
    transform[stdout]: parse: 89.201ms
    transform[stdout]: parse: 94.381ms
    transform[stdout]: parse: 107.966ms
    transform[stdout]: parse: 127.707ms
    transform[stdout]: parse: 411.933ms
    transform[stdout]: parse: 88.575ms
    transform[stdout]: parse: 114.977ms
    transform[stdout]: parse: 119.187ms
    ```
    
    After
    
    ```
    transform[stdout]: parse: 32.369ms
    transform[stdout]: parse: 36.327ms
    transform[stdout]: parse: 46.021ms
    transform[stdout]: parse: 74.98ms
    transform[stdout]: parse: 306.212ms
    transform[stdout]: parse: 33.792ms
    transform[stdout]: parse: 52.749ms
    transform[stdout]: parse: 71.474ms
    ```
    
    Reviewed By: GijsWeterings
    
    Differential Revision: D38868831
    
    Pulled By: robhogan
    
    fbshipit-source-id: 3c1fe323cdc2764fc9e97ac6c8924da000491bd2

--- packages/metro-react-native-babel-transformer/src/index.js
+++ packages/metro-react-native-babel-transformer/src/index.js
@@ -23,7 +23,6 @@ const {parseSync, transformFromAstSync} = require('@babel/core');
 const inlineRequiresPlugin = require('babel-preset-fbjs/plugins/inline-requires');
 const crypto = require('crypto');
 const fs = require('fs');
-const HermesParser = require('hermes-parser');
 const makeHMRConfig = require('metro-react-native-babel-preset/src/configs/hmr');
 const {generateFunctionMap} = require('metro-source-map');
 const nullthrows = require('nullthrows');
@@ -211,7 +210,7 @@ function transform({filename, options, src, plugins}: BabelTransformerArgs): {
       isTSXSource(filename) ||
       !options.hermesParser
         ? parseSync(src, babelConfig)
-        : HermesParser.parse(src, {
+        : require('hermes-parser').parse(src, {
             babel: true,
             sourceType: babelConfig.sourceType,
           });
