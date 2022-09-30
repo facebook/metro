3db59ecf7c640c80e187140ce29699a2286b3d44
Rujin Cao <rujinc@fb.com>
2022-09-29 22:32:43 -0700

    Sanitize docblocks with @oncall (*.js)
    Differential Revision: D39959932
    
    fbshipit-source-id: 2ad05d7a00191f0abe896c89852c96f8d5e70db9

--- packages/metro-file-map/src/crawlers/watchman/__tests__/planQuery-test.js
+++ packages/metro-file-map/src/crawlers/watchman/__tests__/planQuery-test.js
@@ -4,9 +4,9 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
+ * @flow strict
  * @format
  * @oncall react_native
- * @flow strict
  */
 
 import {planQuery} from '../planQuery';
--- packages/metro-runtime/src/polyfills/require.js
+++ packages/metro-runtime/src/polyfills/require.js
@@ -4,10 +4,10 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @polyfill
  * @flow
  * @format
  * @oncall react_native
+ * @polyfill
  */
 
 'use strict';
