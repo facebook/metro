95cfe243276f1401aaeefe992799111c74c540a3
Rob Hogan <robhogan@fb.com>
2022-12-19 02:40:10 -0800

    Crawler integration tests
    Differential Revision: D42122025
    
    fbshipit-source-id: b98b8d853f61771ee6445d35364c71e64bda3f62

---
+++ packages/metro-file-map/src/crawlers/__tests__/integration-test.js
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ * @oncall react_native
+ */
+
+import nodeCrawl from '../node';
+import watchmanCrawl from '../watchman';
+import {execSync} from 'child_process';
+import os from 'os';
+import {join} from 'path';
+import type {CrawlerOptions, FileData} from '../../flow-types';
+
+// At runtime we use a more sophisticated + robust Watchman capability check,
+// but this simple heuristic is fast to check, synchronous (we can't
+// asynchronously skip tests: https://github.com/facebook/jest/issues/8604),
+// and will tend to exercise our Watchman tests whenever possible.
+const isWatchmanOnPath = () => {
+  try {
+    execSync(os.platform() === 'windows' ? 'where watchman' : 'which watchman');
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+type Crawler = (opts: CrawlerOptions) => Promise<{
+  removedFiles: FileData,
+  changedFiles: FileData,
+}>;
+
+const CRAWLERS: {[key: string]: ?Crawler} = {
+  'node-no-force': nodeCrawl,
+  'node-force': (opts: CrawlerOptions) =>
+    nodeCrawl({...opts, forceNodeFilesystemAPI: true}),
+  watchman: isWatchmanOnPath() ? watchmanCrawl : null,
+};
+
+const FIXTURES_DIR = join(__dirname, '..', '__fixtures__');
+
+describe.each(Object.keys(CRAWLERS))(
+  'Crawler integration tests (%s)',
+  crawlerName => {
+    const crawl = CRAWLERS[crawlerName];
+    const maybeTest = crawl ? test : test.skip;
+
+    maybeTest('Finds the expected files', async () => {
+      const result = await crawl({
+        previousState: {
+          files: new Map([['removed.js', ['', 123, 234, 0, '', null]]]),
+          clocks: new Map(),
+        },
+        enableSymlinks: false,
+        extensions: ['js'],
+        ignore: path => path.includes('ignored'),
+        roots: [FIXTURES_DIR],
+        rootDir: FIXTURES_DIR,
+      });
+
+      // Map comparison is unordered, which is what we want
+      expect(result).toMatchObject({
+        changedFiles: new Map([
+          [
+            join('directory', 'bar.js'),
+            ['', expect.any(Number), 245, 0, '', null],
+          ],
+          ['foo.js', ['', expect.any(Number), 245, 0, '', null]],
+        ]),
+        removedFiles: new Map([['removed.js', ['', 123, 234, 0, '', null]]]),
+      });
+      if (crawlerName === 'watchman') {
+        expect(result.clocks).toBeInstanceOf(Map);
+      }
+    });
+  },
+);
