4d21c2e1d1d606e238a1ad5f89fd1a7e7eabd5ec
Deepak Jacob <deepakjacob@meta.com>
2023-03-21 05:36:36 +0000

    Align module config to metro preset

--- packages/metro-swc-transformer/lib/transformer/options.rs
+++ packages/metro-swc-transformer/lib/transformer/options.rs
@@ -1,8 +1,19 @@
 use swc::config::{Config, JsMinifyOptions, JscConfig, ModuleConfig, Options, TransformConfig};
 use swc_config::config_types::{BoolConfig, BoolOrDataConfig};
 use swc_ecma_parser::{EsConfig, Syntax, TsConfig};
+use swc_ecma_transforms_module as module;
 use swc_ecma_transforms_react as react;
 use swc_ecma_utils::swc_ecma_ast::EsVersion;
+
+fn get_module_config() -> ModuleConfig {
+  let module_config = ModuleConfig::CommonJs(module::common_js::Config {
+    strict: false,
+    strict_mode: false,
+    no_interop: false,
+    ..Default::default()
+  });
+  module_config
+}
 
 pub fn get_config_options(is_typescript: bool) -> Options {
   let syntax = match is_typescript {
@@ -39,7 +50,7 @@ pub fn get_config_options(is_typescript: bool) -> Options {
         .into(),
         ..Default::default()
       },
-      module: Some(ModuleConfig::CommonJs(Default::default())),
+      module: Some(get_module_config()),
       ..Default::default()
     },
     ..Default::default()
