b7524593bd037259af39536819317cdf0eae4f38
Alex Hunt <alexeh@meta.com>
2023-03-30 15:06:44 -0700

    Move ESLint config into scripts/eslint
    Summary: Changelog: [Internal]
    
    Reviewed By: motiz88
    
    Differential Revision: D44214576
    
    fbshipit-source-id: 4e21c22b5adafcc5258b7b92b8a81bbeabd9ed1f

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,45 +10,9 @@
 
 'use strict';
 
-const path = require('path');
-
-require('eslint-plugin-lint').load(path.join(__dirname, 'eslint-rules'));
-
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
-  env: {
-    node: true,
-  },
-  root: true,
-  extends: ['eslint-config-fb-strict', 'plugin:prettier/recommended'],
-  plugins: ['babel', 'ft-flow', 'import', 'lint', 'prettier'],
-  parser: 'hermes-eslint',
-  rules: {
-    'babel/quotes': ['error', 'single', 'avoid-escape'],
-    'consistent-return': 'error',
-    'import/no-extraneous-dependencies': 'error',
-    'lint/extra-arrow-initializer': 'off',
-    'lint/strictly-null': 'warn',
-    'max-len': 'off',
-    'no-alert': 'error',
-    'no-console': 'error',
-    'no-unused-vars': 'error',
-    'no-var': 'off',
-    'prefer-const': ['warn', {destructuring: 'all'}],
-    quotes: 'off',
-    'sort-keys': 'off',
-
-    // prettier handles this
-    'flowtype/object-type-delimiter': 'off',
-    'ft-flow/object-type-delimiter': 'off',
-
-    // These rules are not required with hermes-eslint
-    'ft-flow/define-flow-type': 0,
-    'ft-flow/use-flow-type': 0,
-    'flowtype/define-flow-type': 0,
-    'flowtype/use-flow-type': 0,
-    // flow handles this check for us, so it's not required
-    'no-undef': 0,
-  },
+  extends: './scripts/eslint/base',
   overrides: [
     {
       files: ['flow-typed/**/*.js'],
@@ -62,29 +26,12 @@ module.exports = {
       rules: {
         'operator-assignment': ['error', 'never'],
       },
-      env: {
-        node: true,
-      },
     },
     {
       files: ['scripts/**/*.js'],
       rules: {
         'babel/func-params-comma-dangle': 'off',
         'import/no-extraneous-dependencies': 'off',
-      },
-    },
-    {
-      files: ['**/__tests__/**/*.js'],
-      env: {
-        jest: true,
-      },
-      rules: {
-        'babel/quotes': [
-          'error',
-          'single',
-          {avoidEscape: true, allowTemplateLiterals: true},
-        ],
-        quotes: 'off',
       },
     },
   ],
