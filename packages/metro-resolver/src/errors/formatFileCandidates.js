a1eed8587d81d4e38d37c0a8d0fed5f1f3288012
evanbacon <bacon@expo.io>
2023-07-17 18:52:37 -0500

    added tests and support for enterprise extensions

--- packages/metro-resolver/src/errors/formatFileCandidates.js
+++ packages/metro-resolver/src/errors/formatFileCandidates.js
@@ -15,6 +15,59 @@ import path from 'path';
 
 import type {FileCandidates} from '../types';
 
+function groupExtensions(exts: $ReadOnlyArray<string>): string[][] {
+  // Reverse the extensions and split into parts
+  const extParts = exts.map(ext => ext.split('.').reverse());
+
+  // Find the maximum depth of extension parts
+  const maxDepth = Math.max(...extParts.map(parts => parts.length));
+
+  // Initialize groups based on the max depth
+  const groups = Array.from({length: maxDepth}, () => new Set<string>());
+
+  extParts.forEach(parts => {
+    parts.forEach((part, i) => {
+      // Add parts to the corresponding group based on their depth
+      groups[i].add(part);
+    });
+  });
+
+  // Cycle groups and remove duplicates that appear forwards
+  groups.forEach((group, index) => {
+    // Remove duplicates that appear forwards
+    // NOTE: This doesn't support extensions like `.native.native.js`
+    groups.forEach((otherGroup, otherIndex) => {
+      if (index < otherIndex) {
+        otherGroup.forEach(part => group.delete(part));
+      }
+    });
+  });
+
+  // Convert sets back to arrays and reverse groups to correct order
+  return groups.map(group => Array.from(group)).reverse();
+}
+
+function createMatcherPatternForExtensions(
+  exts: $ReadOnlyArray<string>,
+): string {
+  let formatted = '';
+
+  if (exts.length) {
+    // Apply grouping function
+    const groups = groupExtensions(exts);
+
+    formatted += groups
+      .map((group, index) => {
+        return index < groups.length - 1
+          ? `(${group.map(ext => `.${ext}`).join('|')})?`
+          : `.(${group.join('|')})`;
+      })
+      .join('');
+  }
+
+  return formatted;
+}
+
 function formatFileCandidates(
   candidates: FileCandidates,
   allowIndex: boolean = false,
@@ -22,43 +75,20 @@ function formatFileCandidates(
   if (candidates.type === 'asset') {
     return candidates.name;
   }
+
   let formatted = candidates.filePathPrefix;
 
   if (allowIndex) {
     formatted += `(${path.sep}index)?`;
   }
 
-  if (candidates.candidateExts.length) {
-    const exts = candidates.candidateExts.filter(Boolean);
-    // exts are formatted as `.ios.js, .native.js, .js, .native.json, .json`
-    // we want to split them into groups delimited by the period, then format
-    // like `(.native|.ios).(js|json)`
+  const extensions = candidates.candidateExts
+    // Drop additional dots, the first character if it is a dot, and remove empty strings.
+    .map(ext => ext.replace(/\.+/g, '.').replace(/^\./g, ''))
+    .filter(Boolean);
 
-    // split into groups, filter, reverse to ensure we get the most specific
-    const groups = exts.map(ext => ext.split('.').filter(Boolean).reverse());
+  formatted += createMatcherPatternForExtensions(extensions);
 
-    const splits = groups
-      .reduce((acc: Array<string>, group) => {
-        group.forEach((ext, i) => {
-          if (!acc[i]) {
-            acc[i] = [];
-          }
-          acc[i].push(ext);
-        });
-        return acc;
-      }, [])
-      // Remove duplicates
-      .map(group => [...new Set(group)])
-      .reverse();
-
-    formatted += splits
-      .map((split, index) => {
-        return index < splits.length - 1
-          ? `(${split.map(split => `.${split}`).join('|')})?`
-          : `.(${split.join('|')})`;
-      })
-      .join('');
-  }
   return formatted;
 }
 
