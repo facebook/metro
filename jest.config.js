4e5bd945f647b41a5e3151c3d9f5c3c597cba8ba
Vitali Zaidman <vzaidman@gmail.com>
2024-10-15 09:44:28 -0700

    Skip certain tests when installing yarn ignoring the lockfile (#1369)
    Summary:
    1. Allows to create a filter file to programmatically skip selected tests
    2. Uses that filter file to skip the babel types tests on nightlies that is currently failing
    3. The next diff will skip certain tests on Windows so the rest of them can run and pass until all Windows tests are fixed.
    
    Pull Request resolved: https://github.com/facebook/metro/pull/1369
    
    Test Plan:
    Before:
    * Nightlies [don't pass](https://github.com/facebook/metro/actions/runs/11286648134) at the moment on main.
    
    After:
    * Nightlies [**DO pass**](https://github.com/facebook/metro/actions/runs/11289332286/job/31399239374) on this branch since they are ignoring the test `Babel Flow library definitions should be up to date`.
    
    Reviewed By: huntie
    
    Differential Revision: D64231975
    
    Pulled By: vzaidman
    
    fbshipit-source-id: dd574d520646532a93a0ab7c9565b2c406407524

---
+++ jest.config.js
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ * @oncall react_native
+ */
+
+const testPathIgnorePatterns = [];
+if (process.env.NIGHTLY_TESTS_NO_LOCKFILE) {
+  // flaky babel types test - this should be removed once babel is updated
+  testPathIgnorePatterns.push('__tests__/babel-lib-defs-test.js');
+}
+
+/** @type {import('jest').Config} **/
+module.exports = {
+  modulePathIgnorePatterns: ['/node_modules/', 'packages/[^/]+/build/'],
+  snapshotFormat: {
+    escapeString: true,
+    printBasicPrototype: true,
+  },
+  testEnvironment: 'node',
+  testRegex: '/__tests__/.*-test\\.js$',
+  testPathIgnorePatterns,
+  fakeTimers: {
+    enableGlobally: true,
+    legacyFakeTimers: false,
+  },
+  transform: {
+    '\\.js$': '<rootDir>/scripts/babelJestTransformer.js',
+  },
+  setupFiles: ['<rootDir>/scripts/setupJest.js'],
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
+};
