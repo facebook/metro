d733b4b359e7617253b676f02e277ec57122acc0
screetBloom <15306225869@163.com>
2024-10-31 20:05:29 +0800

    feat: optimize getMaxWorkers using os.availableParallelism

--- packages/metro/src/lib/getMaxWorkers.js
+++ packages/metro/src/lib/getMaxWorkers.js
@@ -14,8 +14,7 @@
 const os = require('os');
 
 module.exports = (workers: ?number): number => {
-  const cores = os.cpus().length;
   return typeof workers === 'number' && Number.isInteger(workers)
-    ? Math.min(cores, workers > 0 ? workers : 1)
-    : Math.max(1, Math.ceil(cores * (0.5 + 0.5 * Math.exp(-cores * 0.07)) - 1));
+    ? workers
+    : os.availableParallelism();
 };
