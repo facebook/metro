facebook/metro

commit 61c606606f9cb292ac4cc900b8d687412d01e14f

Return "live" `hasteModuleMap` and `fileSystem` from `metro-file-map`'s `build()`
Summary:
Modify the contract of "build()" in `metro-file-map` to return file system and Haste module map objects that reflect the latest state (at least as up to date as any emitted `ChangeEvent`).

Note the behaviour of `DependencyGraph` that gives us confidence this change is safe for Metro:
 - `DependencyGraph` currently wraps some `_hasteFS` and `_moduleMap` APIs, but it does not expose the objects themselves.
 - `DependencyGraph` updates its internal `_hasteFS` and `_moduleMap` fields whenever it receives a `'change'` event from `metro-file-map`. Thus, they already represent "live" state.
 - Additionally, `ModuleMap` and `FileSystem` expose only read-only APIs.

I've renamed those objects as well, `hasteFS` -> `fileSystem` and `moduleMap` -> `hasteModuleMap`. Two reasons:
 - (IMO) for clarity, as though `hasteFS` (which implements `FileSystem`) has some Haste-related methods, it's used more widely for existence checks, path matching, and as a hash provider. `ModuleMap` on the other hand deals exclusively with Haste modules and Haste packages (and Jest mocks*).
 - Confidence that we're not missing any uses and therefore possible reliance on the snapshot behaviour - the rename would cause a hard failure for anything we haven't addressed.

*Jest is a background consideration here - though Jest is not a dependent at the moment, we are hoping to upstream these changes to `jest-haste-map`. I'm keeping that in mind, and there's nothing changing here that can't be accommodated with minor changes upstream. I'm also planning to remove mocks from `ModuleMap`, because there's no real reason for them to be there, and lift them to a consumer of/subscriber to `jest-haste-map` in Jest.

Changelog: Internal

Reviewed By: motiz88

Differential Revision: D41053143

fbshipit-source-id: bdb2d94ecb15613623c416a2ca3c627c30d519aa

packages/metro-file-map/src/index.js
@@ -364,30 +364,28 @@ export default class HasteMap extends EventEmitter {
           data = initialData;
         }
 
-        const snapshot = deepCloneInternalData(data);
-
-        await this._persist(
-          snapshot,
+        await this._takeSnapshotAndPersist(
+          data,
           fileDelta.changedFiles,
           fileDelta.removedFiles,
         );
 
         const rootDir = this._options.rootDir;
-        const snapshotFS = new HasteFS({
-          files: snapshot.files,
+        const fileSystem = new HasteFS({
+          files: data.files,
           rootDir,
         });
-        const moduleMap = new HasteModuleMap({
-          duplicates: snapshot.duplicates,
-          map: snapshot.map,
-          mocks: snapshot.mocks,
+        const hasteModuleMap = new HasteModuleMap({
+          duplicates: data.duplicates,
+          map: data.map,
+          mocks: data.mocks,
           rootDir,
         });
 
         await this._watch(data);
         return {
-          snapshotFS,
-          moduleMap,
+          fileSystem,
+          hasteModuleMap,
         };
       })();
     }
@@ -763,10 +761,20 @@ export default class HasteMap extends EventEmitter {
   }
 
   /**
-   * 4. serialize the new `HasteMap` in a cache file.
+   * 4. Serialize a snapshot of our raw data via the configured cache manager
    */
-  async _persist(snapshot: InternalData, changed: FileData, removed: FileData) {
+  async _takeSnapshotAndPersist(
+    data: InternalData,
+    changed: FileData,
+    removed: FileData,
+  ) {
     this._startupPerfLogger?.point('persist_start');
+    // We must take a snapshot here synchronously, because `data` is only
+    // internally consistent immediately after crawling - we do not update
+    // `data.clocks` on changes.
+    //
+    // TODO: Make this more explicit
+    const snapshot = deepCloneInternalData(data);
     await this._cacheManager.write(snapshot, {changed, removed});
     this._startupPerfLogger?.point('persist_end');
   }

packages/metro-file-map/src/__tests__/index-test.js
@@ -243,9 +243,9 @@ describe('HasteMap', () => {
   });
 
   it('matches files against a pattern', async () => {
-    const {snapshotFS} = await new HasteMap(defaultConfig).build();
+    const {fileSystem} = await new HasteMap(defaultConfig).build();
     expect(
-      snapshotFS.matchFiles(
+      fileSystem.matchFiles(
         process.platform === 'win32' ? /project\\fruits/ : /project\/fruits/,
       ),
     ).toEqual([
@@ -255,7 +255,7 @@ describe('HasteMap', () => {
       path.join('/', 'project', 'fruits', '__mocks__', 'Pear.js'),
     ]);
 
-    expect(snapshotFS.matchFiles(/__mocks__/)).toEqual([
+    expect(fileSystem.matchFiles(/__mocks__/)).toEqual([
       path.join('/', 'project', 'fruits', '__mocks__', 'Pear.js'),
     ]);
   });
@@ -265,16 +265,16 @@ describe('HasteMap', () => {
     mockFs[path.join('/', 'project', 'fruits', 'Kiwi.js')] = `
       // Kiwi!
     `;
-    const {snapshotFS} = await new HasteMap(config).build();
-    expect(snapshotFS.matchFiles(/Kiwi/)).toEqual([]);
+    const {fileSystem} = await new HasteMap(config).build();
+    expect(fileSystem.matchFiles(/Kiwi/)).toEqual([]);
   });
 
   it('ignores vcs directories without ignore pattern', async () => {
     mockFs[path.join('/', 'project', 'fruits', '.git', 'fruit-history.js')] = `
       // test
     `;
-    const {snapshotFS} = await new HasteMap(defaultConfig).build();
-    expect(snapshotFS.matchFiles('.git')).toEqual([]);
+    const {fileSystem} = await new HasteMap(defaultConfig).build();
+    expect(fileSystem.matchFiles('.git')).toEqual([]);
   });
 
   it('ignores vcs directories with ignore pattern regex', async () => {
@@ -286,9 +286,9 @@ describe('HasteMap', () => {
     mockFs[path.join('/', 'project', 'fruits', '.git', 'fruit-history.js')] = `
       // test
     `;
-    const {snapshotFS} = await new HasteMap(config).build();
-    expect(snapshotFS.matchFiles(/Kiwi/)).toEqual([]);
-    expect(snapshotFS.matchFiles('.git')).toEqual([]);
+    const {fileSystem} = await new HasteMap(config).build();
+    expect(fileSystem.matchFiles(/Kiwi/)).toEqual([]);
+    expect(fileSystem.matchFiles('.git')).toEqual([]);
   });
 
   it('warn on ignore pattern except for regex', async () => {
@@ -1328,20 +1328,20 @@ describe('HasteMap', () => {
       });
     }
 
-    hm_it('provides a new set of hasteHS and moduleMap', async hm => {
+    hm_it('build returns a "live" fileSystem and hasteModuleMap', async hm => {
       const initialResult = await hm.build();
       const filePath = path.join('/', 'project', 'fruits', 'Banana.js');
-      expect(initialResult.snapshotFS.getModuleName(filePath)).toBeDefined();
-      expect(initialResult.moduleMap.getModule('Banana')).toBe(filePath);
+      expect(initialResult.fileSystem.getModuleName(filePath)).toBeDefined();
+      expect(initialResult.hasteModuleMap.getModule('Banana')).toBe(filePath);
       mockDeleteFile(path.join('/', 'project', 'fruits'), 'Banana.js');
       mockDeleteFile(path.join('/', 'project', 'fruits'), 'Banana.js');
       const {eventsQueue, snapshotFS, moduleMap} = await waitForItToChange(hm);
       expect(eventsQueue).toHaveLength(1);
       const deletedBanana = {filePath, stat: undefined, type: 'delete'};
       expect(eventsQueue).toEqual([deletedBanana]);
-      // Verify we didn't change the original map.
-      expect(initialResult.snapshotFS.getModuleName(filePath)).toBeDefined();
-      expect(initialResult.moduleMap.getModule('Banana')).toBe(filePath);
+      // Verify that the initial result has been updated
+      expect(initialResult.fileSystem.getModuleName(filePath)).toBeNull();
+      expect(initialResult.hasteModuleMap.getModule('Banana')).toBeNull();
       expect(snapshotFS.getModuleName(filePath)).toBeNull();
       expect(moduleMap.getModule('Banana')).toBeNull();
     });
@@ -1454,7 +1454,7 @@ describe('HasteMap', () => {
     hm_it(
       'correctly tracks changes to both platform-specific versions of a single module name',
       async hm => {
-        const {moduleMap: initMM} = await hm.build();
+        const {hasteModuleMap: initMM} = await hm.build();
         expect(initMM.getModule('Orange', 'ios')).toBeTruthy();
         expect(initMM.getModule('Orange', 'android')).toBeTruthy();
         const e = mockEmitters[path.join('/', 'project', 'fruits')];

packages/metro-file-map/src/__tests__/includes_dotfiles-test.js
@@ -47,10 +47,10 @@ test('watchman crawler and node crawler both include dotfiles', async () => {
   ]);
 
   expect(
-    builtHasteMapWithWatchman.snapshotFS.matchFiles('.eslintrc.js'),
+    builtHasteMapWithWatchman.fileSystem.matchFiles('.eslintrc.js'),
   ).toHaveLength(1);
 
-  expect(builtHasteMapWithWatchman.snapshotFS.getAllFiles().sort()).toEqual(
-    builtHasteMapWithNode.snapshotFS.getAllFiles().sort(),
+  expect(builtHasteMapWithWatchman.fileSystem.getAllFiles().sort()).toEqual(
+    builtHasteMapWithNode.fileSystem.getAllFiles().sort(),
   );
 });

packages/metro-file-map/src/flow-types.js
@@ -41,8 +41,8 @@ export type BuildParameters = $ReadOnly<{
 }>;
 
 export type BuildResult = {
-  snapshotFS: FileSystem,
-  moduleMap: ModuleMap,
+  fileSystem: FileSystem,
+  hasteModuleMap: ModuleMap,
 };
 
 export interface CacheManager {

packages/metro/src/node-haste/DependencyGraph.js
@@ -12,16 +12,15 @@
 import type Package from './Package';
 import type {ConfigT} from 'metro-config/src/configTypes.flow';
 import type MetroFileMap, {
+  ChangeEvent,
   FileSystem,
+  IModuleMap,
   HealthCheckResult,
   WatcherStatus,
 } from 'metro-file-map';
 import type Module from './Module';
 
-import {
-  DuplicateHasteCandidatesError,
-  ModuleMap as MetroFileMapModuleMap,
-} from 'metro-file-map';
+import {DuplicateHasteCandidatesError} from 'metro-file-map';
 
 const canonicalize = require('metro-core/src/canonicalize');
 const createHasteMap = require('./DependencyGraph/createHasteMap');
@@ -57,9 +56,9 @@ class DependencyGraph extends EventEmitter {
   _assetExtensions: Set<string>;
   _config: ConfigT;
   _haste: MetroFileMap;
-  _snapshotFS: FileSystem;
+  _fileSystem: FileSystem;
   _moduleCache: ModuleCache;
-  _moduleMap: MetroFileMapModuleMap;
+  _hasteModuleMap: IModuleMap;
   _moduleResolver: ModuleResolver<Module, Package>;
   _resolutionCache: Map<
     // Custom resolver options
@@ -112,15 +111,14 @@ class DependencyGraph extends EventEmitter {
     this._haste = haste;
     this._haste.on('status', status => this._onWatcherStatus(status));
 
-    this._readyPromise = haste.build().then(({snapshotFS, moduleMap}) => {
+    this._readyPromise = haste.build().then(({fileSystem, hasteModuleMap}) => {
       log(createActionEndEntry(initializingMetroLogEntry));
       config.reporter.update({type: 'dep_graph_loaded'});
 
-      this._snapshotFS = snapshotFS;
-      this._moduleMap = moduleMap;
+      this._fileSystem = fileSystem;
+      this._hasteModuleMap = hasteModuleMap;
 
-      // $FlowFixMe[method-unbinding] added when improving typing for this parameters
-      this._haste.on('change', this._onHasteChange.bind(this));
+      this._haste.on('change', changeEvent => this._onHasteChange(changeEvent));
       this._haste.on('healthCheck', result =>
         this._onWatcherHealthCheck(result),
       );
@@ -161,7 +159,7 @@ class DependencyGraph extends EventEmitter {
     let dir = parsedPath.dir;
     do {
       const candidate = path.join(dir, 'package.json');
-      if (this._snapshotFS.exists(candidate)) {
+      if (this._fileSystem.exists(candidate)) {
         return candidate;
       }
       dir = path.dirname(dir);
@@ -169,12 +167,8 @@ class DependencyGraph extends EventEmitter {
     return null;
   }
 
-  /* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
-   * LTI update could not be added via codemod */
-  _onHasteChange({eventsQueue, snapshotFS, moduleMap}) {
-    this._snapshotFS = snapshotFS;
+  _onHasteChange({eventsQueue}: ChangeEvent) {
     this._resolutionCache = new Map();
-    this._moduleMap = moduleMap;
     eventsQueue.forEach(({filePath}) => this._moduleCache.invalidate(filePath));
     this._createModuleResolver();
     this.emit('change');
@@ -194,9 +188,9 @@ class DependencyGraph extends EventEmitter {
       emptyModulePath: this._config.resolver.emptyModulePath,
       extraNodeModules: this._config.resolver.extraNodeModules,
       getHasteModulePath: (name, platform) =>
-        this._moduleMap.getModule(name, platform, true),
+        this._hasteModuleMap.getModule(name, platform, true),
       getHastePackagePath: (name, platform) =>
-        this._moduleMap.getPackage(name, platform, true),
+        this._hasteModuleMap.getPackage(name, platform, true),
       isAssetFile: file => this._assetExtensions.has(path.extname(file)),
       mainFields: this._config.resolver.resolverMainFields,
       moduleCache: this._moduleCache,
@@ -210,7 +204,7 @@ class DependencyGraph extends EventEmitter {
           ...this._config.resolver.assetResolutions.map(
             resolution => basePath + '@' + resolution + 'x' + extension,
           ),
-        ].filter(candidate => this._snapshotFS.exists(candidate));
+        ].filter(candidate => this._fileSystem.exists(candidate));
         return assets.length ? assets : null;
       },
       resolveRequest: this._config.resolver.resolveRequest,
@@ -220,13 +214,12 @@ class DependencyGraph extends EventEmitter {
 
   _createModuleCache(): ModuleCache {
     return new ModuleCache({
-      // $FlowFixMe[method-unbinding] added when improving typing for this parameters
-      getClosestPackage: this._getClosestPackage.bind(this),
+      getClosestPackage: filePath => this._getClosestPackage(filePath),
     });
   }
 
   getAllFiles(): Array<string> {
-    return nullthrows(this._snapshotFS).getAllFiles();
+    return nullthrows(this._fileSystem).getAllFiles();
   }
 
   getSha1(filename: string): string {
@@ -246,7 +239,7 @@ class DependencyGraph extends EventEmitter {
     // been talking about for stuff like CSS or WASM).
 
     const resolvedPath = fs.realpathSync(containerName);
-    const sha1 = this._snapshotFS.getSha1(resolvedPath);
+    const sha1 = this._fileSystem.getSha1(resolvedPath);
 
     if (!sha1) {
       throw new ReferenceError(
@@ -278,7 +271,7 @@ class DependencyGraph extends EventEmitter {
       filter: RegExp,
     }>,
   ): string[] {
-    return this._snapshotFS.matchFilesWithContext(from, context);
+    return this._fileSystem.matchFilesWithContext(from, context);
   }
 
   resolveDependency(
@@ -351,11 +344,11 @@ class DependencyGraph extends EventEmitter {
   }
 
   _doesFileExist = (filePath: string): boolean => {
-    return this._snapshotFS.exists(filePath);
+    return this._fileSystem.exists(filePath);
   };
 
   getHasteName(filePath: string): string {
-    const hasteName = this._snapshotFS.getModuleName(filePath);
+    const hasteName = this._fileSystem.getModuleName(filePath);
 
     if (hasteName) {
       return hasteName;
@@ -365,7 +358,7 @@ class DependencyGraph extends EventEmitter {
   }
 
   getDependencies(filePath: string): Array<string> {
-    return nullthrows(this._snapshotFS.getDependencies(filePath));
+    return nullthrows(this._fileSystem.getDependencies(filePath));
   }
 }
 

commit 2a0b548519f97acad1d1809bfdcc9ada5df4d006

Fix edge case where files may briefly "disappear" during processing
Summary:
Currently, when a file is modified on disk
 1. The watcher backend emits a change event
 2. `FileMap` receives the event, and deletes the in-memory record of the file (including from the Haste map, etc)
 3. A worker processes the file
 4. `FileMap` re-adds the file with new metadata

If Metro is currently working (eg, building a bundle) it may attempt to read from the Haste or file maps between 2 and 4, and find that the file appears not to exist, which could fail the build either during resolution or transformation.

This bug was introduced in [61c6066](https://github.com/facebook/metro/commit/61c606606f9cb292ac4cc900b8d687412d01e14f) - prior to that, consumers would only receive snapshots of the data taken at "safe" points.

Instead, it'd better for the file map to return the last known state of the file until it has updated state, as long as all exposed data is self-consistent. With this change, we move all removal logic under the `'delete'` event handler.

```
 - **[Fix]**: Fix edge case where files may appear missing during bundling/resolution while being modified.
```

Reviewed By: huntie

Differential Revision: D67762435

fbshipit-source-id: 85a1799662aa0adc8bfb9a326db93608b85dc97f

packages/metro-file-map/src/index.js
@@ -963,16 +963,6 @@ export default class FileMap extends EventEmitter {
             return null;
           };
 
-          // If it's not an addition, delete the file and all its metadata
-          if (linkStats != null) {
-            this._removeIfExists(
-              fileSystem,
-              hasteMap,
-              mockMap,
-              relativeFilePath,
-            );
-          }
-
           // If the file was added or modified,
           // parse it and update the haste map.
           if (change.event === 'touch') {
@@ -1017,6 +1007,12 @@ export default class FileMap extends EventEmitter {
               // This is expected for deletion of an ignored file.
               return null;
             }
+            this._removeIfExists(
+              fileSystem,
+              hasteMap,
+              mockMap,
+              relativeFilePath,
+            );
             enqueueEvent({
               modifiedTime: null,
               size: null,

packages/metro-file-map/src/__tests__/index-test.js
@@ -62,9 +62,10 @@ jest.mock('../crawlers/watchman', () =>
       ) {
         const relativeFilePath = path.relative(rootDir, file);
         if (list[file]) {
-          const hash = computeSha1 ? mockHashContents(list[file]) : null;
           const isSymlink = typeof list[file].link === 'string';
           if (!isSymlink || includeSymlinks) {
+            const hash =
+              !isSymlink && computeSha1 ? mockHashContents(list[file]) : null;
             changedFiles.set(relativeFilePath, [
               '',
               32,
@@ -152,6 +153,16 @@ jest.mock('fs', () => ({
     }),
   },
 }));
+
+jest.mock('../worker.js', () => ({
+  worker: mockWorkerFn,
+}));
+
+const mockWorkerFn = jest
+  .fn()
+  .mockImplementation((...args) =>
+    jest.requireActual('../worker').worker(...args),
+  );
 
 const object = data => Object.assign(Object.create(null), data);
 const createMap = obj => new Map(Object.entries(obj));
@@ -1676,6 +1687,61 @@ describe('FileMap', () => {
       const {eventsQueue} = await waitForItToChange(hm);
       expect(eventsQueue).toHaveLength(1);
     });
+
+    fm_it(
+      'file data is still available during processing',
+      async hm => {
+        const e = mockEmitters[path.join('/', 'project', 'fruits')];
+        const {fileSystem, hasteMap} = await hm.build();
+        // Pre-existing file
+        const bananaPath = path.join('/', 'project', 'fruits', 'Banana.js');
+        expect(fileSystem.linkStats(bananaPath)).toEqual({
+          fileType: 'f',
+          modifiedTime: 32,
+        });
+        const originalHash = fileSystem.getSha1(bananaPath);
+        expect(typeof originalHash).toBe('string');
+
+        mockFs[bananaPath] = `
+        // Modified banana!
+      `;
+        e.emitFileEvent({
+          event: 'touch',
+          relativePath: 'Banana.js',
+          metadata: {
+            type: 'f',
+            modifiedTime: 33,
+            size: 500,
+          },
+        });
+
+        // Wait for the a worker job to be enqueued, but not yet resolved
+        const initialWorkerCalls = mockWorkerFn.mock.calls.length;
+        await null;
+        expect(mockWorkerFn).toHaveBeenCalledTimes(initialWorkerCalls + 1);
+
+        // Initially, expect same data as before
+        expect(fileSystem.linkStats(bananaPath)).toEqual({
+          fileType: 'f',
+          modifiedTime: 32,
+        });
+        expect(fileSystem.getSha1(bananaPath)).toBe(originalHash);
+        expect(hasteMap.getModule('Banana')).toBe(bananaPath);
+
+        const {eventsQueue} = await waitForItToChange(hm);
+        expect(eventsQueue).toHaveLength(1);
+
+        // After the 'change' event is emitted, we should have new data
+        expect(fileSystem.linkStats(bananaPath)).toEqual({
+          fileType: 'f',
+          modifiedTime: 33,
+        });
+        const newHash = fileSystem.getSha1(bananaPath);
+        expect(typeof newHash).toBe('string');
+        expect(newHash).not.toBe(originalHash);
+      },
+      {config: {computeSha1: true}},
+    );
 
     fm_it(
       'suppresses backend symlink events if enableSymlinks: false',