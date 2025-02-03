a2a25c3c224b8a55295a5499d6501affd729a0b9
Rob Hogan <robhogan@meta.com>
2025-02-03 04:20:58 -0800

    Move Flow 'node' declarations to Metro OSS
    Summary:
    Move some internal Node.js Flow declarations to OSS to allow them to be used in Metro.
    
    Changelog: Internal
    
    Reviewed By: huntie
    
    Differential Revision: D69024870
    
    fbshipit-source-id: 4954aaf5226334523f9b6b62342501414432bd2f

---
+++ flow-typed/node.js
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
+
+declare module 'timers' {
+  declare export class Timeout {
+    close(): this;
+    hasRef(): boolean;
+    ref(): this;
+    refresh(): this;
+    unref(): this;
+    [key: $SymbolToPrimitive]: () => number;
+    // Wait for Node 18.18, and multiple key support
+    // [key: $SymbolDispose]: () => void;
+  }
+
+  declare export class Immediate {
+    hasRef(): boolean;
+    ref(): this;
+    unref(): this;
+    // Wait for Node 18.18
+    // [key: $SymbolDispose]: () => void;
+  }
+
+  declare export function setTimeout<TArgs = $ReadOnlyArray<mixed>>(
+    callback: (...args: TArgs) => mixed,
+    delay: ?number /* default: 1 */,
+    ...args: TArgs
+  ): Timeout;
+  declare export function setInterval<TArgs = $ReadOnlyArray<mixed>>(
+    callback: (...args: TArgs) => mixed,
+    delay: ?number /* default: 1 */,
+    ...args: TArgs
+  ): Timeout;
+  declare export function setImmediate<TArgs = $ReadOnlyArray<mixed>>(
+    callback: (...args: TArgs) => mixed,
+    ...args: TArgs
+  ): Immediate;
+
+  declare export function clearTimeout(timeout: Timeout): void;
+  declare export function clearInterval(timeout: Timeout): void;
+  declare export function clearImmediate(immediate: Immediate): void;
+}
