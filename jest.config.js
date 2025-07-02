f7a7e0b6810e6dc3722092749956aba8b0a6e75d
Rob Hogan <robhogan@meta.com>
2025-07-02 05:57:31 -0700

    Move Windows broken test skipping from Jest config to a filter module (#1525)
    Summary:
    Pull Request resolved: https://github.com/facebook/metro/pull/1525
    
    Currently we use `testPathIgnorePatterns` to skip known-broken tests on some configurations (mainly tests that weren't written for Windows).
    
    Using `testPathIgnorePatterns` ignores the tests on the file-crawling level. This has the side-effect of changing the file map cache key, which means Metro tests on Windows don't benefit from sharing a cache with posix (i.e., Saved State).
    
    This uses the [`filter`](https://jestjs.io/docs/cli#--filterfile) config instead to filter tests at Jest runtime. This may be bypassed with `--skipFilter`.
    
    Changelog: Internal
    
    Reviewed By: vzaidman
    
    Differential Revision: D77656335
    
    fbshipit-source-id: bd9d7a9c70a3d7b522b3d8c6aa5e413f55edeac3

--- jest.config.js
+++ jest.config.js
@@ -8,56 +8,9 @@
  * @oncall react_native
  */
 
-const testPathIgnorePatterns = [];
-if (process.env.NIGHTLY_TESTS_NO_LOCKFILE) {
-  // flaky babel types test - this should be removed once babel is updated
-  testPathIgnorePatterns.push('__tests__/babel-lib-defs-test.js');
-}
-
-if (process.platform === 'win32') {
-  // TODO: fix on windows
-  testPathIgnorePatterns.push(
-    ...[
-      // path mismatches
-      'packages/metro/src/__tests__/HmrServer-test.js',
-      'packages/metro/src/DeltaBundler/__tests__/buildSubgraph-test.js',
-      'packages/metro/src/DeltaBundler/__tests__/Graph-test.js',
-      'packages/metro/src/DeltaBundler/Serializers/helpers/__tests__/js-test.js',
-      'packages/metro/src/node-haste/lib/__tests__/AssetPaths-test.js',
-      'packages/metro/src/Server/__tests__/Server-test.js',
-      'packages/metro-config/src/__tests__/loadConfig-test.js',
-      'packages/metro-symbolicate/src/__tests__/symbolicate-test.js',
-      'packages/metro-file-map/src/__tests__/index-test.js',
-      'packages/metro-file-map/src/crawlers/__tests__/node-test.js',
-
-      // resolveModulePath failed
-      'packages/metro-cache/src/stores/__tests__/FileStore-test.js',
-      'packages/metro-resolver/src/__tests__/assets-test.js',
-      'packages/metro-resolver/src/__tests__/platform-extensions-test.js',
-      'packages/metro-resolver/src/__tests__/symlinks-test.js',
-
-      // const {_cwd} = this; resolution issue in `metro-memory-fs/src/index.js:1294:15`
-      'packages/metro/src/__tests__/Assets-test.js',
-      'packages/metro/src/DeltaBundler/__tests__/resolver-test.js',
-      'packages/buck-worker-tool/src/__tests__/worker-test.js',
-      'packages/metro-transform-worker/src/__tests__/index-test.js',
-      'packages/metro-cache/src/stores/__tests__/AutoCleanFileStore-test.js',
-      'packages/metro-cache/src/stores/__tests__/FileStore-test.js',
-
-      // endless loading
-      'packages/metro-resolver/src/__tests__/browser-spec-test.js',
-      'packages/metro-resolver/src/__tests__/package-exports-test.js',
-      'packages/metro-resolver/src/__tests__/index-test.js',
-
-      // unclear issue
-      'packages/metro/src/DeltaBundler/__tests__/DeltaCalculator-test.js',
-      'packages/metro-file-map/src/crawlers/__tests__/integration-test.js',
-    ],
-  );
-}
-
 /** @type {import('jest').Config} **/
 module.exports = {
+  filter: '<rootDir>/scripts/jestFilter.js',
   modulePathIgnorePatterns: ['/node_modules/', 'packages/[^/]+/build/'],
   snapshotFormat: {
     escapeString: true,
@@ -65,7 +18,6 @@ module.exports = {
   },
   testEnvironment: 'node',
   testRegex: '/__tests__/.*-test\\.js$',
-  testPathIgnorePatterns,
   fakeTimers: {
     enableGlobally: true,
     legacyFakeTimers: false,
