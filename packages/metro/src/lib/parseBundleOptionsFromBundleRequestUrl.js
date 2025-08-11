92ac74c552d82132cb4cda98eea84be819bff9b0
Bradley Ayers <bradley.ayers@gmail.com>
2025-08-11 10:27:06 -0700

    support non-ASCII filesystem bundle paths with metro server (#1538)
    Summary:
    In a codebase with non-ascii characters in file paths (e.g. Japanese or Chinese characters), metro has an error when serving bundles:
    
    ```
    TypeError: Invalid character in header content
    ```
    
    To fix it, we decode the URL received in `parseBundleOptionsFromBundleRequestUrl`.
    
    Notice that this is just a quick fix to the bug, however hot reload and other parts of the system would still not work with non-ascii characters.
    
    This will be fixed in the next diff.
    
    Changelog: [Fixed] Initial metro support to non-ASCII bundle/source files/sourcemaps
    
    Pull Request resolved: https://github.com/facebook/metro/pull/1538
    
    Test Plan:
    * `js1 test parseBundleOptionsFromBundleRequestUrl-test.js`
    * Hot reload works the same with regular ascii filenames
    * Also- using the test diff D78806803 I managed to reproduce Metro not launching RNTester before this fix:
    
    1. `buck2 install -r rntester-android` (with the modified bundle path)
    2. `DEV=1 js1 run`
    3. launch `RNTester`
    
    Before- app crashes:
     https://pxl.cl/7LFkb
    
    After- app launches, debugging works:
    {F1980565661}
    
    Can even add the bundle to ignore-list and then it gets ignored:
    {F1980565660}
    
    Reviewed By: robhogan
    
    Differential Revision: D78799358
    
    Pulled By: vzaidman
    
    fbshipit-source-id: 1d5b1541f594a1a64479a66947528a8ab85a4518

--- packages/metro/src/lib/parseBundleOptionsFromBundleRequestUrl.js
+++ packages/metro/src/lib/parseBundleOptionsFromBundleRequestUrl.js
@@ -46,11 +46,20 @@ export default function parseBundleOptionsFromBundleRequestUrl(
   // Retained for backwards compatibility, unused in Metro, to be removed.
   bundleType: string,
 } {
-  const parsedURL = nullthrows(url.parse(rawNonJscSafeUrlEncodedUrl, true)); // `true` to parse the query param as an object.
+  const relativeProtocolUrl = rawNonJscSafeUrlEncodedUrl.startsWith('//');
+  const parsedURL = nullthrows(
+    url.parse(
+      (relativeProtocolUrl ? 'temp-protocol:' : '') +
+        rawNonJscSafeUrlEncodedUrl,
+      true,
+    ),
+  ); // `true` to parse the query param as an object.
+
   const query = nullthrows(parsedURL.query);
-  const pathname =
-    query.bundleEntry ||
-    (parsedURL.pathname != null ? decodeURIComponent(parsedURL.pathname) : '');
+
+  const pathname = query.bundleEntry || (parsedURL?.pathname ?? '');
+  const decodedPathname = decodeURIComponent(pathname);
+
   const platform =
     query.platform || parsePlatformFilePath(pathname, platforms).platform;
   const bundleType = getBundleType(path.extname(pathname).substr(1));
@@ -61,7 +70,9 @@ export default function parseBundleOptionsFromBundleRequestUrl(
     customTransformOptions: parseCustomTransformOptions(parsedURL),
     dev: getBoolean(query, 'dev', true),
     // absolute and relative paths are converted to paths relative to root
-    entryFile: pathname.replace(/^(?:\.?\/)?/, './').replace(/\.[^/.]+$/, ''),
+    entryFile: decodedPathname
+      .replace(/^(?:\.?\/)?/, './')
+      .replace(/\.[^/.]+$/, ''),
     excludeSource: getBoolean(query, 'excludeSource', false),
     hot: true,
     inlineSourceMap: getBoolean(query, 'inlineSourceMap', false),
