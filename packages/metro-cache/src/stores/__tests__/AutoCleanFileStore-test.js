2b3637e81a5cdb2d85081946070b4e5a452b601d
Rob Hogan <robhogan@fb.com>
2025-09-09 06:47:48 -0700

    metro-cache: Cleanup FileStore caches, deprecate AutoCleanFileStore
    Summary:
    Prefer actually-private class field/method syntax (`#` prefix) over private-by-convention (`_` prefix).
    
    Flow will already error on use of `_`-private fields, but they will generate TypeScript types as if part of the public API.
    
    Also cleans up some inconsistent headers and a stray self-import of a type. This prepares `metro-cache` for fully auto-generated TypeScript types.
    
    Differential Revision: D81051149

--- packages/metro-cache/src/stores/__tests__/AutoCleanFileStore-test.js
+++ packages/metro-cache/src/stores/__tests__/AutoCleanFileStore-test.js
@@ -9,7 +9,7 @@
  * @oncall react_native
  */
 
-'use strict';
+import {memfs} from 'memfs';
 
 describe('AutoCleanFileStore', () => {
   let AutoCleanFileStore;
@@ -19,21 +19,22 @@ describe('AutoCleanFileStore', () => {
     jest
       .resetModules()
       .resetAllMocks()
-      .mock('fs', () => new (require('metro-memory-fs'))());
-
+      .mock('fs', () => memfs().fs);
     AutoCleanFileStore = require('../AutoCleanFileStore').default;
     fs = require('fs');
+    jest.spyOn(fs, 'statSync');
     jest.spyOn(fs, 'unlinkSync');
   });
 
   test('sets and writes into the cache', async () => {
-    // $FlowFixMe[underconstrained-implicit-instantiation]
-    const fileStore = new AutoCleanFileStore({
+    const fileStore = new AutoCleanFileStore<mixed>({
       root: '/root',
       intervalMs: 49,
-      cleanupThresholdMs: 0,
+      cleanupThresholdMs: 90,
     });
     const cache = Buffer.from([0xfa, 0xce, 0xb0, 0x0c]);
+
+    expect(fs.statSync).toHaveBeenCalledTimes(0);
 
     await fileStore.set(cache, {foo: 42});
     expect(await fileStore.get(cache)).toEqual({foo: 42});
@@ -43,17 +44,28 @@ describe('AutoCleanFileStore', () => {
 
     expect(await fileStore.get(cache)).toEqual({foo: 42});
 
+    // And there should have been no cleanup
+    expect(fs.statSync).not.toHaveBeenCalled();
+
     // Run to 50ms so that we've exceeded the 49ms cleanup interval
     jest.advanceTimersByTime(20);
 
-    // mtime doesn't work very well in in-memory-store, so we couldn't test that
-    // functionality
+    expect(fs.statSync).toHaveBeenCalledTimes(1);
+
+    // At 50ms we should have checked the file, but it's still fresh enough
+    expect(await fileStore.get(cache)).toEqual({foo: 42});
+    expect(fs.unlinkSync).not.toHaveBeenCalled();
+
+    jest.advanceTimersByTime(50);
+
+    // After another 50ms, we should have checked the file again and deleted it
+    expect(fs.statSync).toHaveBeenCalledTimes(2);
+    expect(fs.unlinkSync).toHaveBeenCalledTimes(1);
     expect(await fileStore.get(cache)).toEqual(null);
   });
 
   test('returns null when reading a non-existing file', async () => {
-    // $FlowFixMe[underconstrained-implicit-instantiation]
-    const fileStore = new AutoCleanFileStore({root: '/root'});
+    const fileStore = new AutoCleanFileStore<mixed>({root: '/root'});
     const cache = Buffer.from([0xfa, 0xce, 0xb0, 0x0c]);
 
     expect(await fileStore.get(cache)).toEqual(null);
