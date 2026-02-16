de59fcc3bb3e10ae433237cf4f017dbaaf89d338
Rob Hogan <robhogan@meta.com>
2026-02-16 08:05:28 -0800

    Fix transformer changing process.env.BABEL_ENV as a side-effect, transform tests non-determinism
    Summary:
    X-link: https://github.com/facebook/react-native/pull/55557
    
    Currently, the RN Babel transformer that is used both by Metro and Jest, and the base transformer in Metro, set [`process.env.BABEL_ENV`](https://babeljs.io/docs/options?utm_source=chatgpt.com#envname) during transformation, presumably to ensure that Babel always uses the `development` configuration when multiple are configured.
    
    It attempts to set it back in a `finally` block - but that's guarded in the old value being truthy, which was introduced as a fix in https://github.com/facebook/metro/pull/446 - setting `process.env.BABEL_ENV = undefined` actually gives it the string value `'undefined'`.
    
    However, the fix wasn't quite right, because it just leaves a previously-unset `BABEL_ENV` at its new value. So if `BABEL_ENV` is initially `undefined`, using the transformer sets it to `'development'`.
    
    This has a knock-on effect on `react-native/babel-preset`, which sets `dev: true` on the preset if `BABEL_ENV` is `'development'`.
    
    Changelog:
    [General][Fixed] Fix Babel transformer changing `BABEL_ENV` as a side effect
    
    Reviewed By: huntie
    
    Differential Revision: D93328555
    
    fbshipit-source-id: e47f51028e60efc1c346bea44d1d8f2af264ca60

--- packages/metro-babel-transformer/src/index.js
+++ packages/metro-babel-transformer/src/index.js
@@ -130,7 +130,14 @@ function transform({
       metadata: transformResult.metadata,
     };
   } finally {
-    if (OLD_BABEL_ENV) {
+    // Restore the old process.env.BABEL_ENV
+    if (OLD_BABEL_ENV == null) {
+      // We have to treat this as a special case because writing undefined to
+      // an environment variable coerces it to the string 'undefined'. To
+      // unset it, we must delete it.
+      // See https://github.com/facebook/metro/pull/446
+      delete process.env.BABEL_ENV;
+    } else {
       process.env.BABEL_ENV = OLD_BABEL_ENV;
     }
   }
