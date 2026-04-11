c1cea5c6f957002f07758f74844c6d61129d956b
Phil Pluckthun <phil@kitten.sh>
2026-04-11 13:49:38 +0100

    Rebuild ts-defs

---
+++ packages/metro-file-map/types/lib/removeOverlappingRoots.d.ts
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @noformat
+ * @generated SignedSource<<dae8a0ecc5f107c458a914e4a7a0741a>>
+ *
+ * This file was translated from Flow by scripts/generateTypeScriptDefinitions.js
+ * Original file: packages/metro-file-map/src/lib/removeOverlappingRoots.js
+ * To regenerate, run:
+ *   js1 build metro-ts-defs (internal) OR
+ *   yarn run build-ts-defs (OSS) 
+ */
+
+declare function removeOverlappingRoots(
+  roots: ReadonlyArray<string>,
+): ReadonlyArray<string>;
+export default removeOverlappingRoots;
